f5e9cfa779d9ec329147b58bb8eadba0b41d1bcf
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-06-21 19:13:48 +0000

    Updates

---
+++ precache-manifest.6635665672597c2966e183567988ae33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a53bc71eb676afa04c76a46bee846eba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f3f97e41ca1b841c070",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "e631c8010dbb30c50405",
+    "url": "/static/js/2.41179d9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41179d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f3f97e41ca1b841c070",
+    "url": "/static/js/main.fe95d0dc.chunk.js"
+  },
+  {
+    "revision": "ba2d40902d4f9800a133",
+    "url": "/static/js/runtime-main.c214ac1c.js"
+  }
+]);
