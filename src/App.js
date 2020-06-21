a78683a45610f7fd5df5e526de7278d5378cdd96
Terra Andoh Baffoe <terra.andoh@gmail.com>
2020-06-21 21:11:02 +0000

    Feat: Home page created.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import { Container } from "reactstrap";
 
 function App() {
   return (
     <div className="App">
-      <h1>COVID AWARENESS</h1>
+      <div className="overlay">
+        <Container>
+          <Home />
+        </Container>
+      </div>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// Reactstrap
+import { Navbar, Col } from "reactstrap";
+
+const Home = () => {
+    return (
+        <Col md="12" className="Home">
+            <h1 className="home-title text-white">COVID-19 AWARENESS</h1>
+            <h1 className="home-sub-title text-white">FOR THE PEOPLE OF WALEWALE</h1>
+            <h1 className="home-sub-title course-code">CSCD 426</h1>
+        </Col>
+    );
+}
+
+export default Home;
